a84d633d640791f9f387badbbe0943184219139a
gsboylan <gsboylan@gmail.com>
2015-11-10 16:21:42 -0500

    Added user interaction to callback, called main in a less scripty way

--- testWii.py
+++ testWii.py
@@ -19,47 +19,57 @@ import sys
 import pprint
 
 NUM_CONNECTION_TRIES = 5
+wm = None
+
+def main():
+	raw_input('Press Enter to begin...')
+	print('Press 1 and 2 on your Wiimote now.')
+	attempt = 0
+	while not wm:
+		try:
+			print('Attempt ', attempt + 1, ' of ', NUM_CONNECTION_TRIES)
+			wm = cwiid.Wiimote()
+		except RuntimeError:
+			if (attempt < NUM_CONNECTION_TRIES):
+				attempt += 1
+			else:
+				print('Maximum number of attempts reached. Exiting.')
+				quit()
+	print('Connected to wiimote.')
+
+	time.sleep(1)
+	wm.rumble = True
+	wm.led = cwiid.LED1_ON
+	time.sleep(0.5)
+	wm.led = cwiid.LED2_ON
+	time.sleep(0.5)
+	wm.led = cwiid.LED3_ON
+	time.sleep(0.5)
+	wm.led = cwiid.LED4_ON
+	time.sleep(0.5)
+	wm.led = 0
+	wm.rumble = False
+
+	wm.rpt_mode = cwiid.RPT_BTN
+
+	wm.mesg_callback = handle_callback
+	wm.enable(cwiid.FLAG_MESG_IFC)
+
+	print('Press ctrl+c to disconnect and exit.')
+	while True:
+		pass
 
 def handle_callback(msg_list, time):
 	for msg in msg_list:
-		pprint.pprint(msg)
+		if msg[0] == cwiid.MESG_BTN
+			wm.rumble = (msg(1) & cwiid.BTN_B)
 
-raw_input('Press Enter to begin...')
-print('Press 1 and 2 on your Wiimote now.')
-wm = None
-attempt = 0
-while not wm:
-	try:
-		print('Attempt ', attempt + 1, ' of ', NUM_CONNECTION_TRIES)
-		wm = cwiid.Wiimote()
-	except RuntimeError:
-		if (attempt < NUM_CONNECTION_TRIES):
-			attempt += 1
-		else:
-			print('Maximum number of attempts reached. Exiting.')
-			quit()
-print('Connected to wiimote.')
+			if (msg[1] & cwiid.BTN_UP):
+				wm.led += 1
+			elif (msg[1] & cwiid.BTN_DOWN):
+				wm.led -= 1
 
-wm.rumble = True
-wm.led = cwiid.LED1_ON
-time.sleep(0.1)
-wm.led = cwiid.LED2_ON
-time.sleep(0.1)
-wm.led = cwiid.LED3_ON
-time.sleep(0.1)
-wm.led = cwiid.LED4_ON
-time.sleep(0.1)
-wm.led = 0
-wm.rumble = False
-
-wm.rpt_mode = cwiid.RPT_BTN
-
-wm.mesg_callback = handle_callback
-wm.enable(cwiid.FLAG_MESG_IFC)
-
-print('Press ctrl+c to disconnect and exit.')
-while True:
-	pass
-
-wm.close()
+if __name__ == '__main__': main()
+if wm:
+	wm.close()
 
